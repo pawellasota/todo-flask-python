5f75e2f9a2e58474502c82768541fd8f0880656b
pawellasota <plasota76@gmail.com>
2017-03-02 22:12:54 +0100

    styles an visual upgrade done

--- main.py
+++ main.py
@@ -14,7 +14,16 @@ jsglue = JSGlue(app)
 def list_todo_lists():
     """ Shows list of todo items stored in the database.
     """
-    lists = app.config.logged_user.get_lists()
+    lists = g.logged_user.get_lists()
+    if lists:
+        return render_template("lists.html", lists=lists)
+    return redirect(url_for("index"))
+
+@app.route("/user")
+def user():
+    """ Shows list of todo items stored in the database.
+    """
+    lists = g.logged_user.get_lists()
     if lists:
         return render_template("lists.html", lists=lists)
     return redirect(url_for("index"))
@@ -29,61 +38,48 @@ def list_todo_items(choosed_list_id):
 @app.route("/")
 @app.route("/index")
 def index():
-    if "username" in session:
-        if session["type"] == "manager":
-            return redirect("manager")
-        if g.username:
-            return render_template("index.html")
+    if g.logged_user:
+        if g.logged_user.type == "manager":
+            return redirect(url_for("manager"))
         else:
-            return redirect(url_for("login"))
+            return redirect(url_for("user"))
     else:
         return redirect(url_for("login"))
 
 @app.route("/manager", methods=["GET", "POST"])
 def manager():
     if request.args.get("choosed_user"):
-        app.config.logged_user.remove_access_to_list(request.args.get("choosed_user"), request.args.get("choosed_list"))
+        g.logged_user.remove_access_to_list(request.args.get("choosed_user"), request.args.get("choosed_list"))
         return redirect("manager")
     users_list_names = []
-    users_list = app.config.logged_user.get_all_users()
-    full_list = app.config.logged_user.get_all_lists()
+    users_list = g.logged_user.get_all_users()
+    full_list = g.logged_user.get_all_lists()
     for user in users_list:
-        users_list_names.append([user.user_id, app.config.logged_user.get_user_list_names(user.user_id)])
+        users_list_names.append([user.user_id, g.logged_user.get_user_list_names(user.user_id)])
     if request.method == "POST":
-        app.config.logged_user.assign_list_to_user(request.form["user_to_add"], request.form["list_to_add"])
+        g.logged_user.assign_list_to_user(request.form["user_to_add"], request.form["list_to_add"])
         users_list_names = []
         for user in users_list:
-            users_list_names.append([user.user_id, app.config.logged_user.get_user_list_names(user.user_id)])
+            users_list_names.append([user.user_id, g.logged_user.get_user_list_names(user.user_id)])
     return render_template("manager.html", users_list=users_list, user_list_names=users_list_names, full_list=full_list)
 
 @app.before_request
 def before_request():
-    g.username = None
-    g.user_id = None
-    # g.todo_list_id = None
     if "username" in session:
-        g.username = session["username"]
-        g.user_id = session["user_id"]
-        # g.todo_list_id = session["todo_list_id"]
-
-@app.route("/getsession")
-def get_session():
-    if "user" in session:
-        return session["user"]
+        setattr(g, 'logged_user', User.get_user(session['username'], session['password']))
     else:
-        return "Not logged in"
+        setattr(g, 'logged_user', None)
 
 @app.route("/logout")
 def logout():
     session.pop("username", None)
-    session.pop("user_id", None)
-    session.pop("todo_list_id", None)
+    session.pop("password", None)
     return redirect(url_for("login"))
 
 @app.route("/addlist", methods=["GET", "POST"])
 def add_list():
     if request.method == "POST":
-        app.config.logged_user.add_list(request.form["list_name"])
+        g.logged_user.add_list(request.form["list_name"])
         return redirect(url_for("list_todo_lists"))
     return render_template("add_list.html")
 
@@ -144,23 +140,16 @@ def toggle(todo_id, checked):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == "POST":
-        # if "username" in session:
-        #     session.pop("username", None)
-        #     session.pop("user_id", None)
-        #     session.pop("todo_list_id", None)
-        #     session.pop("password", None)
-        user = User.get_user(request.form["username"], request.form["password"])
-        if user:
-            session["username"] = user.username
-            session["type"] = user.type
-            session["user_id"] = user.user_id
-            # session["todo_list_id"] = user.todo_list_id
-            app.config.logged_user = user
+        logged_user = User.get_user(request.form["username"], request.form["password"])
+        if logged_user:
+            session["username"] = logged_user.username
+            session["password"] = logged_user.password
             return redirect(url_for("index"))
         else:
-            return redirect(url_for("login"))
-    return render_template("login.html")
+            error = "Your login data was incorect"
+    return render_template("login.html", error=error)
 
 if __name__ == "__main__":
     app.run(debug=True)
